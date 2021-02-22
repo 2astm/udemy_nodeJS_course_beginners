1a7769f768b0c5ed2d04d38a334054b477151620
Yevhen Molchaniuk <ymolchaniuk@kiev.luxoft.com>
2021-02-22 17:45:41 +0200

    lessons 1-13

---
+++ refs/os_ref.js
@@ -0,0 +1,22 @@
+const os = require('os')
+
+//platform
+console.log(os.platform());
+
+//architecture
+console.log(os.arch())
+
+// Information
+console.log(os.cpus())
+
+// Free memory
+console.log(os.freemem())
+
+// All memory
+console.log(os.totalmem())
+
+// Root direcotry
+console.log(os.homedir())
+
+// Uptime
+console.log(os.uptime())
