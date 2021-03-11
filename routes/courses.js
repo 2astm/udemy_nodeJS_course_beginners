59e1b459ffbef65eb15f5da7e613c7df96b8499f
Yevhen Molchaniuk <ymolchaniuk@kiev.luxoft.com>
2021-03-11 13:21:56 +0200

    lesson 30

--- routes/courses.js
+++ routes/courses.js
@@ -4,11 +4,28 @@ const router = Router()
 
 router.get('/', async (req,res) => {
     const courses = await Course.getAll()
+    console.log(courses);
     res.render('courses', {
         title: 'Курсы',
         courses: courses,
         isCourses: true
     })
+})
+
+router.get('/:id/edit', async (req,res) => {
+    if (!req.query.allow) {
+        return res.redirect('/')
+    }
+    const course = await Course.getById(req.params.id)
+    res.render('course-edit', {
+        title: `Редактировать ${course.title}`,
+        course
+    })
+})
+
+router.post('/edit', async (req, res) => {
+    await Course.update(req.body)
+    res.redirect('/courses')
 })
 
 router.get('/:id', async (req,res)=>{
